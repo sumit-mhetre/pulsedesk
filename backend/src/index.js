70dbe6145fbb57d6b6b00329284e0bd598cccad6
sumit-mhetre <mhetresumit53@gmail.com>
2026-04-19 23:00:07 +0530

    updating index.js file

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,10 +9,7 @@ const app = express();
 
 // ── Middleware ─────────────────────────────────
 app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://pulsedesk-1.onrender.com'
-  ],
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173'||'https://pulsedesk-1.onrender.com/login',
   credentials: true,
 }));
 app.use(express.json({ limit: '10mb' }));
